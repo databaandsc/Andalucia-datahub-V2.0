f062db6dadfa1578e358c95b2c658f5a4d817dbe
Ricardo Fernández Chamorro <databaandsc@gmail.com>
2026-09-23 11:30:31 +0200

    feat(api): endpoints completos de sectores con busqueda individual y errores 404

--- api/routers/sectores.py
+++ api/routers/sectores.py
@@ -1,23 +1,39 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
 from api import models, schemas
 from api.database import get_db
 
-# Instanciamos el router
+# 1. Instanciamos el router
 router = APIRouter(
     prefix="/api/v1/sectores",
     tags=["Sectores"] # Organiza el Swagger
 )
 
-# 3. Creamos un segundo endpoint para verificar el funcionamiento del sistema
+
+# 2. Endpoint para listar TODOS los sectores
+
 @router.get("/", response_model=list[schemas.SectorResponse])
-def obtener_sectores(db: Session = Depends(get_db), limite: int = 10):
+def obtener_sectores(db: Session = Depends(get_db)):
     """
     Devuelve una lista de registros de sectores, limitados por defecto a 10 resultados
     para no sobrecargar la respuesta.
     """
     # 1. Hacemos la consulta a PostgreSQL usando el modelo
-    sectores = db.query(models.Sector).limit(limite).all()
-    
-    # 2. Devolvemos los datos. FastAPI y Pydantic se encargan de transformarlos al JSON del schema
+    sectores = db.query(models.Sector).all()
     return sectores
+
+# 3. Endpoint para buscar UN sector por su código
+
+@router.get("/{codigo}", response_model=list[schemas.SectorResponse])
+def obtener_sector_por_codigo(codigo: str, db: Session = Depends(get_db)):
+    """"
+    Devuelve un registro de sector filtrado por su codigo
+    """
+    sector = db.query(models.Sector).filter(models.Sector.codigo_sector == codigo).first()
+    # Manejo de errores: Si el sector no existe en la base de datos, lanzamos un 404
+    if not sector:
+        raise HTTPException(status_code=404, detail=f"El sector con código '{codigo}' no existe.")
+        
+    return sector
+    
+    
